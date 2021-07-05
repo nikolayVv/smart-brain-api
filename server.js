805e114ff75409f6769458e8f529c61d140d885a
nikolayVv <nikolaivalvasilev@gmail.com>
2021-07-05 13:59:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,16 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 const port = process.env.PORT || 3000;
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
     client: 'pg',
     connection: {
         connectionString: process.env.DATABASE_URL,
-        ssl: true
+        ssl: {
+            rejectUnauthorized: false
+        }
+        
     }
 });
 
