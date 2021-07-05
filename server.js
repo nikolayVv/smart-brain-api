4da699995669a128b6d2e3eb39ef37d9d13900bf
nikolayVv <nikolaivalvasilev@gmail.com>
2021-07-05 13:56:21 +0200

    adding DB

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const port = process.env.PORT || 3000;
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: '!db!test',
-        database: 'smart-brain'
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
